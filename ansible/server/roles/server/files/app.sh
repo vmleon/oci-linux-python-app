9fffae5af14f7858bfb31da2098de295d58a65d5
Victor Martin <victor.martin.alvarez@oracle.com>
2022-04-09 09:20:21 +0100

    Remove virtualenv, use global python3, still systemd code=exited, status=203/EXEC

--- ansible/server/roles/server/files/app.sh
+++ ansible/server/roles/server/files/app.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# source /home/opc/pythonapp/venv/bin/activate
-# FIXME failing miserably with systemd, it works with ./app.sh
-/home/opc/pythonapp/venv/bin/gunicorn \
+python3 -m gunicorn \
   --bind 0.0.0.0:3000 \
   -w 2 \
-  --enable-stdio-inheritance \
-  wsgi:app \
-  2>&1 > /home/opc/pythonapp/app.log
+  wsgi:app
